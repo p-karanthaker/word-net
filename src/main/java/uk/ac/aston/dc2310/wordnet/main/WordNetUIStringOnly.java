386b585a5552798b417e327ed83a75dcd3a766a9
Karan Thaker <karan.thaker@capgemini.com>
2016-07-11 11:10:11 +0100

    Replaced old TUI with updated version

--- src/main/java/uk/ac/aston/dc2310/wordnet/main/WordNetUIStringOnly.java
+++ src/main/java/uk/ac/aston/dc2310/wordnet/main/WordNetUIStringOnly.java
@@ -3,16 +3,9 @@
  */
 package uk.ac.aston.dc2310.wordnet.main;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import uk.ac.aston.dc2310.wordnet.data.POS;
-import uk.ac.aston.dc2310.wordnet.data.Synset;
-import uk.ac.aston.dc2310.wordnet.data.WordSense;
 
 /**
  * A simple text-based user interface for looking up from WordNet 3.0:
@@ -20,17 +13,19 @@ import uk.ac.aston.dc2310.wordnet.data.WordSense;
  * - meanings of a word in a specified part-of-speech
  * - hypernyms of a word
  * 
+ * NOTE: This UI works with string inputs and outputs only.
+ * 
  * @author S H S Wong
- * @version 01/06/2016
+ * @version 24/06/2016
  */
-public class WordNetUI {
+public class WordNetUIStringOnly {
 
 	private static final Pattern RE_LOOK_UP_WORD_IN_POS = Pattern.compile("ws -(n|v|adj|adv) (.+)");
 	private static final Pattern RE_LOOK_UP_WORD = Pattern.compile("ws (.+)");
 	private static final Pattern RE_LOOK_UP_ALL_HYP = Pattern.compile("hyp -all ([0-9]+)");
 	private static final Pattern RE_LOOK_UP_HYP = Pattern.compile("hyp (.+)");
 	
-	private WNController controller;
+	private WNControllerStringOnly controller;
 	private Scanner in;
 	
 	/**
@@ -38,7 +33,7 @@ public class WordNetUI {
 	 * 
 	 * @param controller	the controller to be used with this UI
 	 */
-	public WordNetUI(WNController controller) {
+	public WordNetUIStringOnly(WNControllerStringOnly controller) {
 		
 		this.controller = controller;
 		
@@ -76,111 +71,31 @@ public class WordNetUI {
 		if (matcher_RE_LOOK_UP_WORD_IN_POS.matches()) {
 			String category = matcher_RE_LOOK_UP_WORD_IN_POS.group(1);      // the required syntactic category
 			String word = matcher_RE_LOOK_UP_WORD_IN_POS.group(2);          // the lexical unit to be looked up
-			POS pos = POS.valueOf(category.toUpperCase());
 			// looks up the meanings of a word in a specified part-of-speech
-			Set<WordSense> meanings = controller.getMeanings(word, pos);
-			if (meanings != null) {
-				displayWordSenses(meanings, "Meanings for {" + word + "} as" + pos.determiner() + pos.knownAs() + ":\n");
-			}
-			else {
-				display("No meaning for " + word + " as a " + category + " found!\n");
-			}
+			String results = controller.getMeanings(word, category);
+			display(results);
 		}
 		else if (matcher_RE_LOOK_UP_WORD.matches()) {
 			String word = matcher_RE_LOOK_UP_WORD.group(1);          // the lexical unit to be looked up
 			// looks up all of the known meanings of the given lexical unit
-			Set<WordSense> meanings = controller.getMeanings(word);
-			if (meanings != null) {
-				displayWordSenses(meanings, "All known meanings for {" + word + "}:\n");
-			}
-			else {
-				display("No known meanings for {" + word + "} found!\n");
-			}
+			String results = controller.getMeanings(word);
+			display(results);
 		}
 		else if (matcher_RE_LOOK_UP_ALL_HYP.matches()) {
 			String synsetId = matcher_RE_LOOK_UP_ALL_HYP.group(1);         // the synset ID to be looked up        
 			// looks up all hypernyms of a specified synset
-			List<Synset> allHypernyms = controller.getAllHypernyms(synsetId);
-			String info = "";
-			if (allHypernyms.isEmpty()) {
-				info += "No known hypernyms for Synset ID " + synsetId + " found!\n";
-			}
-			else {
-				info = "All known hypernyms for Synset ID " + synsetId + ":\n";
-				int count = 0;
-				for (Synset hypernym : allHypernyms) {
-					for (int i=0; i<count; i++) {
-						info += '\t';
-					}
-					info += String.format("(%s) [%s] %s: ", 
-							hypernym.id(), 
-							hypernym.pos().toString(), 
-							hypernym.meaning());
-					Iterator<WordSense> itr = hypernym.words().iterator();
-					while (itr.hasNext()) {
-						WordSense ws = itr.next();
-						info += String.format("%s (%d)", ws.word(), ws.senseNum());
-						if (itr.hasNext()) {
-							info += ", ";
-						}
-						else {
-							info += '\n';
-						}
-					}
-					count++;
-				}
-			}
-			display(info);
+			String results = controller.getAllHypernyms(synsetId);
+			display(results);
 		}
 		else if (matcher_RE_LOOK_UP_HYP.matches()) {
 			String word = matcher_RE_LOOK_UP_HYP.group(1);         // the lexical unit to be looked up
 			// looks up all hypernyms of a word
-			Set<Synset> hyperSynsets = controller.getDirectHypernyms(word);
-			String info = "";
-			if (hyperSynsets != null) {
-				info += "All known direct hypernyms for {" + word + "}:\n";
-				int count = 1;
-				for (Synset s : hyperSynsets) {
-					info += String.format("%2d. (%s) [%s]\t%s%n", 
-							count, s.id(), s.pos().toString(), s.meaning());
-					for (WordSense ws : s.words()) {
-						info += String.format("\t\t\t%s (%d) %n",ws.word(), ws.senseNum());
-					}
-					info += "\n";
-					count++;
-				}
-			}
-			else {
-				info += "No known direct hypernyms for {" + word + "} found!\n";
-			}
-			display(info);
+			String results = controller.getDirectHypernyms(word);
+			display(results);
 		}
 		else {  // Not a recognised command.
 			display(unrecogniseCommandErrorMsg(command));
 		}
-	}
-
-	/**
-	 * Displays the specified word senses for the user to view.
-	 * 
-	 * @param meanings	word senses to be displayed on the screen
-	 * @param header	a header explaining the nature of the info to be displayed
-	 */
-	private void displayWordSenses(Set<WordSense> meanings, String header) {
-		String info = header;
-		int count = 1;
-		for (WordSense ws : meanings) {
-			info += String.format("%2d. (%s) [%s-%d]\t%s", 
-					count, ws.owningSynset().id(), ws.owningSynset().pos().toString(), 
-					ws.senseNum(),
-					ws.owningSynset().meaning());
-			if (!ws.owningSynset().sampleSentences().equals("")) {
-				info += "\n\t\t\t\tAS IN: " +  ws.owningSynset().sampleSentences();
-			}
-			info += "\n";
-			count++;
-		}
-		display(info);
 	}
 	
 	/**
@@ -207,7 +122,7 @@ public class WordNetUI {
 	 */
 	private String menu() {
 		return "ws {word} : lookup the meaning(s) of the specified word\n" + 
-			   "ws -p {pos} {word} : lookup the meaning(s) of the specified word in the given part-of-speech (ie noun, verb, adjective, adverb)\n" +
+			   "ws -{pos} {word} : lookup the meaning(s) of the specified word in the given part-of-speech (i.e. n, v, adj or adv)\n" +
 			   "hyp {word} : lookup the direct hypernyms of the specified word\n" + 
 			   "hyp -all {synsetID} : lookup all hypernyms of the specified synset\n" +
 			   "q : quit\n";
