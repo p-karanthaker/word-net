7667e376d040753ae3f4ec6b99edfb019b916d1c
Karan Thaker <karan.thaker@capgemini.com>
2016-07-13 16:30:10 +0100

    Created PartOfSpeech enum

---
+++ src/main/java/uk/ac/aston/dc2310/wordnet/dictionary/PartOfSpeech.java
@@ -0,0 +1,58 @@
+package uk.ac.aston.dc2310.wordnet.dictionary;
+
+/**
+ * PartOfSpeech shows the parts of speech which are
+ * used within this implementation of WordNet and the
+ * value which maps the first digit in the Synset ID
+ * to the part-of-speech.
+ * 
+ * @author Prem-Karan Thaker
+ *
+ */
+public enum PartOfSpeech {
+
+	/**
+	 * Noun - a noun Synset ID will start with 1.
+	 */
+	N(1),
+	
+	/**
+	 * Verb - a verb Synset ID will start with 2.
+	 */
+	V(2),
+	
+	/**
+	 * Adjective - an adjective Synset ID will start with 3.
+	 */
+	ADJ(3),
+	
+	/**
+	 * Adverb - an adverb Synset ID will start with 4.
+	 */
+	ADV(4);
+	
+	/**
+	 * The value which maps the first digit in the
+	 * Synset ID to the part-of-speech.
+	 */
+	private int value;
+	
+	/**
+	 * Constructor for a PartOfSpeech. Pass the value which is assigned.
+	 * 
+	 * @param value	The value assigned to the part of speech. 
+	 */
+	private PartOfSpeech(int value) {
+		this.value = value;
+	}
+	
+	/**
+	 * Get the value which maps the first digit in the Synset
+	 * ID to the part-of-speech.
+	 * 
+	 * @return the value of the part-of-speech
+	 */
+	public int getValue() {
+		return this.value;
+	}
+}
