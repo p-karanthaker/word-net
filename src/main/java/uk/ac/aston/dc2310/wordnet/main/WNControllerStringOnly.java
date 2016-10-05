fb91702c6c05850ede68171ae678a1b29bceaf89
Karan Thaker <karan.thaker@capgemini.com>
2016-10-05 11:36:58 +0100

    Updated all code to conform to Google Style conventions

--- src/main/java/uk/ac/aston/dc2310/wordnet/main/WNControllerStringOnly.java
+++ src/main/java/uk/ac/aston/dc2310/wordnet/main/WNControllerStringOnly.java
@@ -6,49 +6,53 @@ package uk.ac.aston.dc2310.wordnet.main;
  * @author S H S Wong
  * @version 21/06/2016
  */
-public interface WNControllerStringOnly {
+public interface WnControllerStringOnly {
 
-	/**
-	 * Returns a set of senses (i.e. meanings) for a specified word.
-	 * 
-	 * @param word	a sequence of characters corresponding to an English word
-	 * @return	a String representation of a set of word senses representing the meanings of the specified word; a simple String message if no meanings found.
-	 */
-	public abstract String getMeanings(String word);
+  /**
+   * Returns a set of senses (i.e. meanings) for a specified word.
+   * 
+   * @param word a sequence of characters corresponding to an English word
+   * @return a String representation of a set of word senses representing the meanings of the
+   *         specified word; a simple String message if no meanings found.
+   */
+  public abstract String getMeanings(String word);
 
-	/**
-	 * Returns a set of senses (i.e. meanings) for a specified word in a specified 
-	 * syntactic category (i.e. part-of-speech such as noun, verb, adjective and adverb).
-	 * 
-	 * @param word	a sequence of characters corresponding to an English word
-	 * @param pos	a specified part-of-speech (noun, verb, adjective and adverb)
-	 * @return	a String representation of a set of word senses representing the meanings of the specified word in a specified part-of-speech; a simple String if no meanings found.
-	 */
-	public abstract String getMeanings(String word, String pos);
+  /**
+   * Returns a set of senses (i.e. meanings) for a specified word in a specified syntactic category
+   * (i.e. part-of-speech such as noun, verb, adjective and adverb).
+   * 
+   * @param word a sequence of characters corresponding to an English word
+   * @param pos a specified part-of-speech (noun, verb, adjective and adverb)
+   * @return a String representation of a set of word senses representing the meanings of the
+   *         specified word in a specified part-of-speech; a simple String if no meanings found.
+   */
+  public abstract String getMeanings(String word, String pos);
 
-	/**
-	 * Returns a set of direct hypernyms for a specified word.
-	 * A hypernym is a more general concept of a word. A direct hypernym of a word is
-	 * the least general concept that is linked to the specified word within WordNet.
-	 * E.g. 'motor vehicle' is a hypernym of 'car' in WordNet 3.0.
-	 * 
-	 * @param word	a sequence of characters corresponding to an English word
-	 * @return	a String representation of a set of synsets representing the direct hypernyms of the specified word; a simple String message if no direct hypernyms are found
-	 */
-	public abstract String getDirectHypernyms(String word);
+  /**
+   * Returns a set of direct hypernyms for a specified word. A hypernym is a more general concept of
+   * a word. A direct hypernym of a word is the least general concept that is linked to the
+   * specified word within WordNet. E.g. 'motor vehicle' is a hypernym of 'car' in WordNet 3.0.
+   * 
+   * @param word a sequence of characters corresponding to an English word
+   * @return a String representation of a set of synsets representing the direct hypernyms of the
+   *         specified word; a simple String message if no direct hypernyms are found
+   */
+  public abstract String getDirectHypernyms(String word);
 
-	/**
-	 * Returns a list of synsets which correspond to the hypernyms of the synset as
-	 * specified by the given synset ID. The synsets in the result list are ordered
-	 * according to how general a concept it is when compared with the specified synset,
-	 * i.e. the less general a concept, the closer to the front of the list, with the 
-	 * direct hypernym appearing at the first element in the result list.
-	 * 
-	 * Returns an empty list if no hypernym can be found for the specified synset.
-	 * 
-	 * @param synsetId
-	 * @return	a String representation of a list of Synset objects sorted in an ascending order of their generality; a simple String message if no hypernym for the specified synset is found; 
-	 */
-	public abstract String getAllHypernyms(String synsetId);
+  /**
+   * Returns a list of synsets which correspond to the hypernyms of the synset as specified by the
+   * given synset ID. The synsets in the result list are ordered according to how general a concept
+   * it is when compared with the specified synset, i.e. the less general a concept, the closer to
+   * the front of the list, with the direct hypernym appearing at the first element in the result
+   * list.
+   * 
+   * <p>Returns an empty list if no hypernym can be found for the specified synset.
+   * 
+   * @param synsetId the synsetId to get all hypernyms for
+   * @return a String representation of a list of Synset objects sorted in an ascending order of
+   *         their generality; a simple String message if no hypernym for the specified synset is
+   *         found;
+   */
+  public abstract String getAllHypernyms(String synsetId);
 
 }
--- src/main/java/uk/ac/aston/dc2310/wordnet/main/WordNetUIStringOnly.java
+++ src/main/java/uk/ac/aston/dc2310/wordnet/main/WordNetUIStringOnly.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package uk.ac.aston.dc2310.wordnet.main;
 
 import java.util.Scanner;
@@ -8,131 +5,128 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * A simple text-based user interface for looking up from WordNet 3.0:
- * - meanings of a word
- * - meanings of a word in a specified part-of-speech
- * - hypernyms of a word
+ * A simple text-based user interface for looking up from WordNet 3.0: - meanings of a word -
+ * meanings of a word in a specified part-of-speech - hypernyms of a word
  * 
- * NOTE: This UI works with string inputs and outputs only.
+ * <p>NOTE: This UI works with string inputs and outputs only.
  * 
  * @author S H S Wong
  * @version 24/06/2016
  */
-public class WordNetUIStringOnly {
+public class WordNetUiStringOnly {
 
-	private static final Pattern RE_LOOK_UP_WORD_IN_POS = Pattern.compile("ws -(n|v|adj|adv) (.+)");
-	private static final Pattern RE_LOOK_UP_WORD = Pattern.compile("ws (.+)");
-	private static final Pattern RE_LOOK_UP_ALL_HYP = Pattern.compile("hyp -all ([0-9]+)");
-	private static final Pattern RE_LOOK_UP_HYP = Pattern.compile("hyp (.+)");
-	
-	private WNControllerStringOnly controller;
-	private Scanner in;
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param controller	the controller to be used with this UI
-	 */
-	public WordNetUIStringOnly(WNControllerStringOnly controller) {
-		
-		this.controller = controller;
-		
-		while (true) {
-			displayMenu();
-			getAndProcessUserOption();
-		}
-	}
-	
-	/**
-	 * Displays the header of this application and a summary of menu options.
-	 */
-	private void displayMenu() {
-		display(header());
-		display(menu());
-	}
-	
-	/**
-	 * Obtains an user option and processes it.
-	 */
-	private void getAndProcessUserOption() {
-		in = new Scanner(System.in);
-		String command = in.nextLine();
-		command = command.trim();      // Trims all leading and ending whitespaces.
-		if (command.equalsIgnoreCase("q")) {
-			in.close();
-			// exits the application
-			display("Thank you for using WordNet 3.0. Goodbye!");
-			System.exit(0);
-		} 
-		Matcher matcher_RE_LOOK_UP_WORD = RE_LOOK_UP_WORD.matcher(command);
-		Matcher matcher_RE_LOOK_UP_WORD_IN_POS = RE_LOOK_UP_WORD_IN_POS.matcher(command);
-		Matcher matcher_RE_LOOK_UP_HYP = RE_LOOK_UP_HYP.matcher(command);
-		Matcher matcher_RE_LOOK_UP_ALL_HYP = RE_LOOK_UP_ALL_HYP.matcher(command);
-		if (matcher_RE_LOOK_UP_WORD_IN_POS.matches()) {
-			String category = matcher_RE_LOOK_UP_WORD_IN_POS.group(1);      // the required syntactic category
-			String word = matcher_RE_LOOK_UP_WORD_IN_POS.group(2);          // the lexical unit to be looked up
-			// looks up the meanings of a word in a specified part-of-speech
-			String results = controller.getMeanings(word, category);
-			display(results);
-		}
-		else if (matcher_RE_LOOK_UP_WORD.matches()) {
-			String word = matcher_RE_LOOK_UP_WORD.group(1);          // the lexical unit to be looked up
-			// looks up all of the known meanings of the given lexical unit
-			String results = controller.getMeanings(word);
-			display(results);
-		}
-		else if (matcher_RE_LOOK_UP_ALL_HYP.matches()) {
-			String synsetId = matcher_RE_LOOK_UP_ALL_HYP.group(1);         // the synset ID to be looked up        
-			// looks up all hypernyms of a specified synset
-			String results = controller.getAllHypernyms(synsetId);
-			display(results);
-		}
-		else if (matcher_RE_LOOK_UP_HYP.matches()) {
-			String word = matcher_RE_LOOK_UP_HYP.group(1);         // the lexical unit to be looked up
-			// looks up all hypernyms of a word
-			String results = controller.getDirectHypernyms(word);
-			display(results);
-		}
-		else {  // Not a recognised command.
-			display(unrecogniseCommandErrorMsg(command));
-		}
-	}
-	
-	/**
-	 * Returns an error message for an unrecognised command.
-	 * 
-	 * @param error	the unrecognised command
-	 * @return	an error message
-	 */
-	private String unrecogniseCommandErrorMsg(String error) {
-		return String.format("Cannot recognise the given command: %s.%n", error);
-	}
-	
-	/**
-	 * Returns a string representation of a brief title for this application as the header.
-	 * @return	a header
-	 */
-	private String header() {
-		return "WordNet 3.0 - meanings and hypernyms look up\n";
-	}
-	
-	/**
-	 * Returns a string representation of the user menu.
-	 * @return	the user menu
-	 */
-	private String menu() {
-		return "ws {word} : lookup the meaning(s) of the specified word\n" + 
-			   "ws -{pos} {word} : lookup the meaning(s) of the specified word in the given part-of-speech (i.e. n, v, adj or adv)\n" +
-			   "hyp {word} : lookup the direct hypernyms of the specified word\n" + 
-			   "hyp -all {synsetID} : lookup all hypernyms of the specified synset\n" +
-			   "q : quit\n";
-	}
-	
-	/**
-	 * Displays the specified info for the user to view.
-	 * @param info	info to be displayed on the screen
-	 */
-	private void display(String info) {
-		System.out.println(info);
-	}
+  private static final Pattern RE_LOOK_UP_WORD_IN_POS = Pattern.compile("ws -(n|v|adj|adv) (.+)");
+  private static final Pattern RE_LOOK_UP_WORD = Pattern.compile("ws (.+)");
+  private static final Pattern RE_LOOK_UP_ALL_HYP = Pattern.compile("hyp -all ([0-9]+)");
+  private static final Pattern RE_LOOK_UP_HYP = Pattern.compile("hyp (.+)");
+
+  private WnControllerStringOnly controller;
+  private Scanner in;
+
+  /**
+   * Constructor.
+   * 
+   * @param controller the controller to be used with this UI
+   */
+  public WordNetUiStringOnly(WnControllerStringOnly controller) {
+
+    this.controller = controller;
+
+    while (true) {
+      displayMenu();
+      getAndProcessUserOption();
+    }
+  }
+
+  /**
+   * Displays the header of this application and a summary of menu options.
+   */
+  private void displayMenu() {
+    display(header());
+    display(menu());
+  }
+
+  /**
+   * Obtains an user option and processes it.
+   */
+  private void getAndProcessUserOption() {
+    in = new Scanner(System.in);
+    String command = in.nextLine();
+    command = command.trim(); // Trims all leading and ending whitespaces.
+    if (command.equalsIgnoreCase("q")) {
+      in.close();
+      // exits the application
+      display("Thank you for using WordNet 3.0. Goodbye!");
+      System.exit(0);
+    }
+    Matcher matcherReLookUpWord = RE_LOOK_UP_WORD.matcher(command);
+    Matcher matcherReLookUpWordInPos = RE_LOOK_UP_WORD_IN_POS.matcher(command);
+    Matcher matcherReLookUpHyp = RE_LOOK_UP_HYP.matcher(command);
+    Matcher matcherReLookUpAllHyp = RE_LOOK_UP_ALL_HYP.matcher(command);
+    if (matcherReLookUpWordInPos.matches()) {
+      String category = matcherReLookUpWordInPos.group(1); // the required syntactic category
+      String word = matcherReLookUpWordInPos.group(2); // the lexical unit to be looked up
+      // looks up the meanings of a word in a specified part-of-speech
+      String results = controller.getMeanings(word, category);
+      display(results);
+    } else if (matcherReLookUpWord.matches()) {
+      String word = matcherReLookUpWord.group(1); // the lexical unit to be looked up
+      // looks up all of the known meanings of the given lexical unit
+      String results = controller.getMeanings(word);
+      display(results);
+    } else if (matcherReLookUpAllHyp.matches()) {
+      String synsetId = matcherReLookUpAllHyp.group(1); // the synset ID to be looked up
+      // looks up all hypernyms of a specified synset
+      String results = controller.getAllHypernyms(synsetId);
+      display(results);
+    } else if (matcherReLookUpHyp.matches()) {
+      String word = matcherReLookUpHyp.group(1); // the lexical unit to be looked up
+      // looks up all hypernyms of a word
+      String results = controller.getDirectHypernyms(word);
+      display(results);
+    } else { // Not a recognised command.
+      display(unrecogniseCommandErrorMsg(command));
+    }
+  }
+
+  /**
+   * Returns an error message for an unrecognised command.
+   * 
+   * @param error the unrecognised command
+   * @return an error message
+   */
+  private String unrecogniseCommandErrorMsg(String error) {
+    return String.format("Cannot recognise the given command: %s.%n", error);
+  }
+
+  /**
+   * Returns a string representation of a brief title for this application as the header.
+   * 
+   * @return a header
+   */
+  private String header() {
+    return "WordNet 3.0 - meanings and hypernyms look up\n";
+  }
+
+  /**
+   * Returns a string representation of the user menu.
+   * 
+   * @return the user menu
+   */
+  private String menu() {
+    return "ws {word} : lookup the meaning(s) of the specified word\n"
+        + "ws -{pos} {word} : lookup the meaning(s) of the specified word in the given "
+        + "part-of-speech (i.e. n, v, adj or adv)\n"
+        + "hyp {word} : lookup the direct hypernyms of the specified word\n"
+        + "hyp -all {synsetID} : lookup all hypernyms of the specified synset\n" + "q : quit\n";
+  }
+
+  /**
+   * Displays the specified info for the user to view.
+   * 
+   * @param info info to be displayed on the screen
+   */
+  private void display(String info) {
+    System.out.println(info);
+  }
 }
